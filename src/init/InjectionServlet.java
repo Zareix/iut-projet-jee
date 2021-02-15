49b81db8e7c51ee1f9aefb750abb1299d3640f4c
Zareix <raphcatarino@gmail.com>
2021-02-15 19:19:44 +0100

    init (see loadonstartup.txt du prof)

---
+++ src/init/InjectionServlet.java
@@ -0,0 +1,24 @@
+package init;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(value="/initializeResources", loadOnStartup=1)
+public class InjectionServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	public void init() throws ServletException {
+		System.out.println("%%%%%%%%%%%%%%%%%%%%");
+		try {
+			Class.forName("persistantdata.MediatekData");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+}
